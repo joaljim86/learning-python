0d0507f6d463b41d5a64ae636f28f94c9cb0c792
Alexander S <joaljim86@gmail.com>
2024-01-26 21:51:55 -0500

    upload basiccalc and welcome

---
+++ basic_calc.py
@@ -0,0 +1,35 @@
+'''Script title:basic calculator v1
+Jhonny Alexander Soto
+Date: 18-01-2024'''
+
+'''Descripción:
+this calculator let to get the result of 4 basic
+math operations for two numbers (variables).
+'''
+
+#INPUTS (two static variables)
+num1 = 20
+num2 = 3
+
+#PROCES
+add = num1 + num2
+sub = num1 - num2
+nul = num1 * num2
+div = num1 / num2
+
+#OUTPUTS
+print (":::BASIC CALC V1:::")
+print ("Number 1", num1, type(num1))
+print ("Number 2", num2, type(num2))
+print ("the addition is: ", add, type(add))
+print ("the substraction is: ", sub, type(sub))
+print ("the multiplication is: ", nul, type(nul))
+print ("the division is: ", div, type(div))
+print ("\n")
+print (":::BASIC CALC V2:::")
+print ("Number 1", num1,)
+print ("Number 2", num2,)
+print ("the addition is: ", num1+num2)
+print ("the substraction is: ", num1-num2)
+print ("the multiplication is: ", num1*num2)
+print ("the division is: ", num1/num2)
---
+++ welcome.py
@@ -0,0 +1,6 @@
+name = "Alex"
+country = "Pasto"
+
+print ("you´re", name, "and you´re from", country)
+print ("you´re", name)
+print ("you´re from", country)
